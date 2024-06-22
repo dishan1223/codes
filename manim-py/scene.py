f95e0a8b19fece616e31752b1ae5fe2ec2d2a5df
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-06-22 16:16:24 +0600

    comments with namings

--- manim-py/scene.py
+++ manim-py/scene.py
@@ -1,9 +1,13 @@
 #code execution
-# manim -pql scene.py createCircle(name of the class)
+# manim -pql scene.py CreateCircle(name of the class)
+
+# .set_fill() can be use to access some particular property of mobject
+
+# there can be multiple classes to animate differect items
 
 from manim import *
 
-class createCircle(Scene):
+class CreateCircle(Scene):
     def construct(self):
         circle = Circle() # create a circle
         circle.set_fill(GREEN, opacity=0.5) # set color and transparency
