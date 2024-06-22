7d8e812de4fb2e0c3bec83d5c2321b2bb5f8dce0
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-06-22 16:10:05 +0600

    initials

---
+++ manim-py/scene.py
@@ -0,0 +1,10 @@
+#code execution
+# manim -pql scene.py createCircle(name of the class)
+
+from manim import *
+
+class createCircle(Scene):
+    def construct(self):
+        circle = Circle() # create a circle
+        circle.set_fill(PINK, opacity=0.5) # set color and transparency
+        self.play(Create(circle)) # show circle to the scene after rendering it
