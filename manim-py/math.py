ccd288296cc0f19437a59330ae31717b29cab29f
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-07-02 18:47:19 +0600

    hehh

--- manim-py/math.py
+++ manim-py/math.py
@@ -3,7 +3,7 @@ from manim import *
 class MathEquations(Scene):
     def construct(self):
         # MathTex or Tex for math equations
-        equation = MathTex(r"E = mc^2") # r means its a row string for latex
+        equation = Text("E = mc²") # r means its a row string for latex
         
         # postion it to the center/ origin
         equation.move_to(ORIGIN)
